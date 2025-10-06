5f21853e7893b11a6e1a5d2c1cbc4d1a9edd1177
ericfr1tzenvalle <ericfritzenvalle@gmail.com>
2025-10-06 11:19:31 -0300

    build: configuração inicial do spring boot

---
+++ src/main/java/com/ifrs/conectatcc/ConectaTccApplication.java
@@ -0,0 +1,13 @@
+package com.ifrs.conectatcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifrs/conectaTCC/ConectaTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifrs.conectaTCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
