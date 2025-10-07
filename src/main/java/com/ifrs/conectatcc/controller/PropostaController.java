82f16bb8c619cfe5d9c7d44ecc8269919f85066f
ericfr1tzenvalle <ericfritzenvalle@gmail.com>
2025-10-07 07:33:43 -0300

    feat(Proposta): implementar todo processo de criar proposta, buscar, listarTodas e listar as suas, bem como poder deletar e atualizar.

---
+++ src/main/java/com/ifrs/conectatcc/controller/PropostaController.java
@@ -0,0 +1,65 @@
+package com.ifrs.conectatcc.controller;
+
+import com.ifrs.conectatcc.dto.PropostaDTO;
+import com.ifrs.conectatcc.dto.PropostaDetalheDTO;
+import com.ifrs.conectatcc.model.Professor;
+import com.ifrs.conectatcc.model.PropostaTCC;
+import com.ifrs.conectatcc.service.PropostaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/propostas")
+public class PropostaController {
+
+    private final PropostaService propostaService;
+
+    public PropostaController(PropostaService propostaService){
+        this.propostaService = propostaService;
+    }
+
+    //TODO: criar filtros para propostas posteriormente no Service
+
+    @PostMapping("/criar")
+    public ResponseEntity<PropostaDTO> criar(@RequestBody PropostaDTO propostaDTO){
+        PropostaTCC proposta = new PropostaTCC();
+        proposta.setTitulo(propostaDTO.titulo());
+        proposta.setDescricao(propostaDTO.descricao());
+        PropostaDTO propostaCriada = propostaService.criarProposta(proposta);
+        return ResponseEntity.ok(propostaCriada);
+
+    }
+
+    @GetMapping("minhas/{id}")
+    public ResponseEntity<PropostaDetalheDTO> buscarPorId(@PathVariable Long id){
+        PropostaDetalheDTO proposta = propostaService.buscarPorId(id);
+        return ResponseEntity.ok(proposta);
+    }
+
+
+    @GetMapping("/todas")
+    public ResponseEntity<List<PropostaDTO>> listarPropostas(){
+        List<PropostaDTO> propostas = propostaService.listarTodasPropostas();
+        return ResponseEntity.ok(propostas);
+    }
+
+    @GetMapping("/minhas")
+    public ResponseEntity<List<PropostaDTO>> listarPropostasDoProfessorLogado() {
+        List<PropostaDTO> propostas = propostaService.listarPropostasDoProfessorLogado();
+        return ResponseEntity.ok(propostas);
+    }
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<PropostaDTO> atualizarProposta(@PathVariable Long id, @RequestBody PropostaTCC propostaAtualizada) {
+        PropostaDTO propostaAtualizadaNoBanco = propostaService.atualizarProposta(id, propostaAtualizada);
+        return ResponseEntity.ok(propostaAtualizadaNoBanco);
+    }
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<PropostaDTO> deletarProposta(@PathVariable Long id) {
+        propostaService.deletarProposta(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+}
---
+++ src/main/java/com/ifrs/conectatcc/service/PropostaService.java
@@ -0,0 +1,130 @@
+package com.ifrs.conectatcc.service;
+
+import com.ifrs.conectatcc.dto.CandidaturaDTO;
+import com.ifrs.conectatcc.dto.PropostaDTO;
+import com.ifrs.conectatcc.dto.PropostaDetalheDTO;
+import com.ifrs.conectatcc.model.Candidatura;
+import com.ifrs.conectatcc.model.Professor;
+import com.ifrs.conectatcc.model.PropostaTCC;
+import com.ifrs.conectatcc.model.StatusTCC;
+import com.ifrs.conectatcc.repository.PropostaRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class PropostaService {
+    private final PropostaRepository propostaRepository;
+    //Todos autowired foram retirados por não serem uma boa prática.
+    public PropostaService(PropostaRepository propostaRepository) {
+        this.propostaRepository = propostaRepository;
+    }
+
+    @Transactional
+    public List<PropostaDTO> listarTodasPropostas(){
+        List<PropostaTCC> todasPropostas = propostaRepository.findAll();
+        return todasPropostas.stream().map(p-> new PropostaDTO(p.getId(),p.getTitulo(),p.getDescricao(),p.getStatus(),p.getProfessorAutor().getNome())).toList();
+    }
+    @Transactional
+    public List<PropostaDTO> listarPropostasDoProfessorLogado() {
+        Professor professorLogado = (Professor) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return professorLogado.getPropostas()
+                .stream()
+                .map(p -> new PropostaDTO(p.getId(), p.getTitulo(), p.getDescricao(), p.getStatus(), professorLogado.getNome()))
+                .toList();
+    }
+
+    @Transactional
+    public PropostaDTO criarProposta(@Valid PropostaTCC proposta){
+        Professor professorLogado = (Professor) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        proposta.setProfessorAutor(professorLogado);
+        proposta.setStatus(StatusTCC.DISPONIVEL);
+        PropostaTCC salva = propostaRepository.save(proposta);
+        return new PropostaDTO(
+                salva.getId(),
+                salva.getTitulo(),
+                salva.getDescricao(),
+                salva.getStatus(),
+                salva.getProfessorAutor().getNome()
+        );
+    }
+
+
+    @Transactional
+    public PropostaDTO atualizarProposta(Long id, @Valid PropostaTCC propostaAtualizada){
+        Professor professorLogado = (Professor) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        PropostaTCC propostaExistente = propostaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Proposta não encontrada"));
+
+        if(!propostaExistente.getProfessorAutor().equals(professorLogado)){
+            throw new RuntimeException("Você não tem permissão para editar essa proposta");
+        }
+        if (propostaExistente.getStatus() != StatusTCC.DISPONIVEL) {
+            throw new RuntimeException("Só é possível editar propostas com status 'Disponível'.");
+        }
+
+        propostaExistente.setTitulo(propostaAtualizada.getTitulo());
+        propostaExistente.setDescricao(propostaAtualizada.getDescricao());
+        PropostaTCC salva = propostaRepository.save(propostaExistente);
+
+        return new PropostaDTO(
+                salva.getId(),
+                salva.getTitulo(),
+                salva.getDescricao(),
+                salva.getStatus(),
+                salva.getProfessorAutor().getNome()
+        );
+    }
+
+    @Transactional
+    public void deletarProposta(Long id){
+        Professor professorLogado = (Professor) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        PropostaTCC propostaExistente = propostaRepository.findById(id).orElseThrow(() -> new RuntimeException("Proposta não encontrada"));
+        if(!propostaExistente.getProfessorAutor().equals(professorLogado)){
+            throw new RuntimeException("Você não tem permissão para deletar essa proposta");
+        }
+        if(propostaExistente.getStatus() != StatusTCC.DISPONIVEL){
+            throw new RuntimeException("Só é possível deletar propostas com status 'Disponível'.");
+        }
+        propostaRepository.delete(propostaExistente);
+
+    }
+
+    @Transactional
+    public PropostaDetalheDTO buscarPorId(Long id){
+        PropostaTCC proposta = propostaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Proposta não encontrada"));
+
+        List<CandidaturaDTO> candidaturasDTO = proposta.getCandidaturas().stream()
+                .map(c -> new CandidaturaDTO(
+                        c.getId(),
+                        c.getDataCandidatura(),
+                        c.getAlunoCandidato().getNome(),
+                        c.getAlunoCandidato().getEmail(),
+                        c.getAlunoCandidato().getCurso(),
+                        c.getMensagem(),
+                        proposta.getId(),
+                        proposta.getTitulo(),
+                        proposta.getStatus().name()
+                ))
+                .toList();
+
+        return new PropostaDetalheDTO(
+                proposta.getId(),
+                proposta.getTitulo(),
+                proposta.getDescricao(),
+                proposta.getStatus().name(),
+                proposta.getProfessorAutor().getNome(),
+                proposta.getAlunoResponsavel() != null ? proposta.getAlunoResponsavel().getNome() : null,
+                candidaturasDTO
+        );
+    }
+
+
+
+}
