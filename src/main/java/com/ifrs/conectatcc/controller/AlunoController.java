aee3b751ff0b7150f2352e6a3e1f702c80b7101e
ericfr1tzenvalle <ericfritzenvalle@gmail.com>
2025-10-07 07:26:59 -0300

    feat(AlunoController, ProfessorController): implementar lógica para professor aceitarCandidaturas e verPerfil, atualizarPerfil assim como o Aluno

--- src/main/java/com/ifrs/conectatcc/controller/AlunoController.java
+++ src/main/java/com/ifrs/conectatcc/controller/AlunoController.java
@@ -1,30 +1,36 @@
 package com.ifrs.conectatcc.controller;
 
+import com.ifrs.conectatcc.dto.AtualizarPerfilDTO;
+import com.ifrs.conectatcc.dto.PerfilDTO;
 import com.ifrs.conectatcc.repository.PropostaRepository;
+import com.ifrs.conectatcc.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/aluno")
 public class AlunoController {
-    @Autowired
-    private PropostaRepository propostaRepository;
 
-    //TODO: Implementar os endpoints para o AlunoController conforme o AlunoService
-    // Funcionalidades necessarias: //Lembrar de verificar se o aluno esta autenticado e se é o dono da candidatura
-    //TODO: Aluno deve poder se candidatar a uma proposta (mudar status para "EM_ANALISE" e adicionar o aluno como candidato)
-    //TODO: Aluno deve poder desistir de uma proposta (mudar status para "DISPONIVEL" e remover a associacao do aluno com a proposta)
+    private final UserService userService;
 
-    // Filtros necessarios: // talvez criar um endpoint /propostas com query params para os filtros ou controller separado pra propostas
-    //TODO: Aluno deve poder listar suas propostas candidatas (status = "EM_ANALISE" ou "ACEITA")
-    //TODO: Aluno deve listar as propostas disponiveis (status = DISPONIVEL)
-    //TODO: Aluno deve poder listar todas as propostas (independente do status)
-    //TODO: Aluno deve poder buscar propostas por professor orientador
-    //TODO: Aluno deve poder buscar propostas por conhecimento necessario
-    //TODO: Aluno deve poder buscar propostas por titulo
-    // Configurações da conta
-    //TODO: Aluno deve poder editar seus dados (nome, email, telefone e senha) criar ENDPOINT para configurações
-    //TODO: Aluno deve poder excluir sua conta (deve remover a associacao com as propostas que ele tinha se candidatado, mudando o status para "DISPONIVEL" e removendo a associacao do aluno com a proposta)
+    public AlunoController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/perfil")
+    public ResponseEntity<PerfilDTO> verPerfil(){
+        return ResponseEntity.ok(userService.verPerfil());
+    }
+
+    @PutMapping("/perfil")
+    public ResponseEntity<PerfilDTO> atualizarPerfil(@Valid @RequestBody AtualizarPerfilDTO atualizarPerfilDTO){
+        return ResponseEntity.ok(userService.atualizarPerfil(atualizarPerfilDTO));
+    }
+
+
+
 }
--- src/main/java/com/ifrs/conectatcc/controller/ProfessorController.java
+++ src/main/java/com/ifrs/conectatcc/controller/ProfessorController.java
@@ -1,6 +1,11 @@
 package com.ifrs.conectatcc.controller;
 
+import com.ifrs.conectatcc.dto.AtualizarPerfilDTO;
+import com.ifrs.conectatcc.dto.PerfilDTO;
 import com.ifrs.conectatcc.repository.PropostaRepository;
+import com.ifrs.conectatcc.service.CandidaturaService;
+import com.ifrs.conectatcc.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,26 +16,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/professor")
 public class ProfessorController {
-    @Autowired
-    private PropostaRepository propostaRepository;
+    private final CandidaturaService candidaturaService;
+    private final UserService userService;
 
-    //TODO: Implementar ROTAS aqui de acordo com o Service ProfessorService
-    //TODO: O professor deve ser capaz de criar, editar e deletar propostas de TCC
-    //TODO: O professor deve ser capaz de listar suas propostas de TCC
-    //TODO: O professor deve ser capaz de buscar propostas com base em critérios como título, área de conhecimento, etc.
-    //TODO: O professor deve ser capaz de ver as candidaturas dos alunos para suas propostas
-    //TODO: O professor deve ser capaz de aprovar ou rejeitar candidaturas dos alunos para suas propostas, e quando aprovar, o status da proposta deve ser atualizado para "Em Andamento" e o
-    // aluno deve ser notificado e atribuido ao TCC.
+    public ProfessorController(CandidaturaService candidaturaService, UserService userService) {
+        this.candidaturaService = candidaturaService;
+        this.userService = userService;
+    }
+    @PostMapping("/candidaturas/{id}/aceitar")
+    public ResponseEntity<Void> aceitarCandidatura(@PathVariable Long id){
+        candidaturaService.aceitarCandidatura(id);
+        return ResponseEntity.noContent().build();
+    }
 
-    //TODO: O professor deve ser capaz de ver o progresso dos TCCs que ele está supervisionando (OPCICIONAL)
-    //TODO: O professor deve ser capaz de avaliar e fornecer feedback sobre os TCCs que ele está supervisionando (OPCIONAL)
-    //TODO: O professor deve ser capaz de comunicar-se com os alunos que estão trabalhando em seus TCCs (OPCIONAL)
+    @GetMapping("/perfil")
+    public ResponseEntity<PerfilDTO> verPerfil() {
+        return ResponseEntity.ok(userService.verPerfil());
+    }
 
-    //TODO: O professor deve ser capaz de editar seu perfil bem como ver suas informações
-    //TODO: O professor deve ser capaz de ver notificações relacionadas às suas propostas e TCCs supervisionados (OPCIONAL)
-    //TODO: O professor deve ser capaz de ver um dashboard com uma visão geral de suas propostas, TCCs supervisionados e outras informações relevantes
-    //TODO: O professor deve ser capaz de alterar sua senha e outras configurações de conta
-    //TODO: o professor deve ser capaz de deletar sua conta.
+    @PutMapping("/perfil")
+    public ResponseEntity<PerfilDTO> atualizarPerfil(@Valid @RequestBody AtualizarPerfilDTO atualizarPerfilDTO) {
+        return ResponseEntity.ok(userService.atualizarPerfil(atualizarPerfilDTO));
+    }
+
 
 
 }
